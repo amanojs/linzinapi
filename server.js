b4ca2ab801ce3ca54a1c8d21e5d377d6a0bb3e01
amanojs <takashivue07@gmail.com>
2020-06-23 01:20:59 +0900

    setting

--- server.js
+++ server.js
@@ -1,30 +1,7 @@
 const express = require("express"),
   app = express(),
-  port = 443,
+  port = 23450,
   bodyParser = require("body-parser");
-
-var http = require("http");
-var https = require("spdy");
-
-var LEX = require("letsencrypt-express");
-
-var DOMAIN = "linzin.net";
-var EMAIL = "takashivue@gmai.com";
-
-var lex = LEX.create({
-  configDir: require("os").homedir() + "/letsencrypt/etc",
-  approveRegistration: function (hostname, approve) {
-    // leave `null` to disable automatic registration
-    if (hostname === DOMAIN) {
-      // Or check a database or list of allowed domains
-      approve(null, {
-        domains: [DOMAIN],
-        email: EMAIL,
-        agreeTos: true,
-      });
-    }
-  },
-});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -47,34 +24,8 @@ const allowCrossDomain = (req, res, next) => {
 
 app.use(allowCrossDomain);
 
-// リダイレクト処理
-function redirectHttp() {
-  http
-    .createServer(
-      LEX.createAcmeResponder(lex, function redirectHttps(req, res) {
-        res.setHeader("Location", "https://" + req.headers.host + req.url);
-        res.statusCode = 302;
-        res.end("<!-- Hello Developer Person! Please use HTTPS instead -->");
-      })
-    )
-    .listen(80);
-}
+const routes = require("./api/routes/userRoutes");
+routes(app);
 
-// 通常の443接続
-function serveHttps() {
-  const routes = require("./api/routes/userRoutes");
-  routes(app);
-
-  app.use("/", function (req, res) {
-    res.end("Hello World!");
-  });
-
-  https
-    .createServer(lex.httpsOptions, LEX.createAcmeResponder(lex, app))
-    .listen(443);
-}
-
-redirectHttp();
-serveHttps();
-
-console.log("REST API server started on " + "443");
+app.listen(port);
+console.log("REST API server started on " + port);
