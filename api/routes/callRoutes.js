d63b7a848b770bcf92387a2c13a535174f28c51a
amanojs <takashivue07@gmail.com>
2020-08-31 18:00:11 +0900

    websocket route

--- api/routes/callRoutes.js
+++ api/routes/callRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const chalk = require("chalk")
 const debug = require("debug")("app:callRoutes")
 
 const callRouter = express.Router()
@@ -6,7 +7,13 @@ const callRouter = express.Router()
 module.exports = () => {
 
   callRouter.ws("/", (ws, req) => {
-    debug(ws)
+    ws.on("open", () => {
+      debug("[1]connect:" + ws)
+    })
+
+    ws.on("close", () => {
+      debug(chalk.cyan("[1]close"))
+    })
   })
 
   return callRouter
