3dc6be243a8018fd7a5d451a16f12b4e7f5a031c
amanojs <takashivue07@gmail.com>
2020-08-31 18:14:10 +0900

    ws test

--- api/routes/callRoutes.js
+++ api/routes/callRoutes.js
@@ -1,20 +1,41 @@
 const express = require("express")
+const db = require("../config/mysql")
 const chalk = require("chalk")
 const debug = require("debug")("app:callRoutes")
 
 const callRouter = express.Router()
 
+let clients = []
+
 module.exports = () => {
 
   callRouter.ws("/", (ws, req) => {
+    debug("[1]connect:" + ws)
+
+    if (req.query.type === "partner") {
+      clients.push({ ws })
+    }
+
     ws.on("open", () => {
       debug("[1]connect:" + ws)
     })
 
     ws.on("close", () => {
+      clients = clients.filter((client) => {
+        return client.ws !== ws
+      })
       debug(chalk.cyan("[1]close"))
+      debug("clients", clients)
     })
   })
 
   return callRouter
 }
+
+/*
+
+create table waiting(
+  email varchar(256)
+);
+
+*/
