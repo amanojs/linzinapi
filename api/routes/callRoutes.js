468a4c1840e216075110d5534f712631e7542645
takashiVue <takashivue@gmail.com>
2020-09-04 16:18:44 +0900

    通話状況リアルタイム管理

--- api/routes/callRoutes.js
+++ api/routes/callRoutes.js
@@ -2,6 +2,7 @@ const express = require("express")
 const db = require("../config/mysql")
 const chalk = require("chalk")
 const debug = require("debug")("app:callRoutes")
+const moment = require("moment")
 
 const callRouter = express.Router()
 
@@ -17,9 +18,23 @@ module.exports = () => {
     if (req.query.type === "partner" && "email" in req.query) {
       waitclients.push({ ws, email: req.query.email })
       debug(req.query.email)
+      const sql = "INSERT INTO waiting VALUES(?,0);"
+      db.query(sql, [req.query.email], (err, result) => {
+        if (err) {
+          throw err
+        }
+        debug(result)
+      })
     }
 
     ws.on("close", () => {
+      const sql = "DELETE FROM waiting WHERE email = ?;"
+      db.query(sql, [req.query.email], (err, result) => {
+        if (err) {
+          throw err
+        }
+        debug(result)
+      })
       waitclients = waitclients.filter((client) => {
         return client.ws !== ws
       })
@@ -29,18 +44,37 @@ module.exports = () => {
   })
 
   callRouter.ws("/room", (ws, req) => {
-    debug("room connect:" + ws)
+    debug("room connect:" + req.query.peerId)
 
     if (req.query.type === "partner") {
       runclients.push({ ws })
+    }
+
+    if ("peerId" in req.query) {
+      const now = moment().toDate()
+      const sql = "INSERT INTO running VALUES(null,?,?,null);"
+      db.query(sql, [req.query.email, now], (err, result) => {
+        if (err) {
+          throw err
+        }
+      })
     }
 
     ws.on("close", () => {
       runclients = runclients.filter((client) => {
         return client.ws !== ws
       })
+      const now = moment().toDate()
+      console.log('now', now)
+      const sql = "UPDATE running SET end = ? WHERE email = ? AND end IS NULL;"
+      db.query(sql, [now, req.query.email], (err, result) => {
+        if (err) {
+          throw err
+        }
+        debug(result)
+      })
       debug(chalk.cyan("[1]close"))
-      debug("clients", runclients)
+      debug("clients", req.query.peerId)
     })
   })
 
@@ -51,7 +85,6 @@ module.exports = () => {
 
 create table waiting(
   email varchar(256) PRIMARY KEY,
-  sdp varchar(5000) NOT NULL,
   flag Int(1)
 );
 
@@ -59,9 +92,7 @@ create table waiting(
 create table running(
   runId Int(16) PRIMARY KEY AUTO_INCREMENT,
   email varchar(256) NOT NULL,
-  sdp varchar(5000) NOT NULL,
-  clinet varchar(5000) NOT NULL,
-  start date NOT NULL,
-  end date
+  start datetime NOT NULL,
+  end datetime
 );
 */
