6d1c4499b68d41c668eefce52a4287ceae7e4998
takashiVue <takashivue@gmail.com>
2020-09-04 14:05:29 +0900

    ws client

--- api/routes/callRoutes.js
+++ api/routes/callRoutes.js
@@ -5,27 +5,42 @@ const debug = require("debug")("app:callRoutes")
 
 const callRouter = express.Router()
 
-let clients = []
+let waitclients = []
+
+let runclients = []
 
 module.exports = () => {
 
-  callRouter.ws("/", (ws, req) => {
-    debug("[1]connect:" + ws)
+  callRouter.ws("/wait", (ws, req) => {
+    debug("wait connect")
 
-    if (req.query.type === "partner") {
-      clients.push({ ws })
+    if (req.query.type === "partner" && "email" in req.query) {
+      waitclients.push({ ws, email: req.query.email })
+      debug(req.query.email)
     }
 
-    ws.on("open", () => {
-      debug("[1]connect:" + ws)
-    })
-
     ws.on("close", () => {
-      clients = clients.filter((client) => {
+      waitclients = waitclients.filter((client) => {
         return client.ws !== ws
       })
       debug(chalk.cyan("[1]close"))
-      debug("clients", clients)
+      debug("clients", waitclients)
+    })
+  })
+
+  callRouter.ws("/room", (ws, req) => {
+    debug("room connect:" + ws)
+
+    if (req.query.type === "partner") {
+      runclients.push({ ws })
+    }
+
+    ws.on("close", () => {
+      runclients = runclients.filter((client) => {
+        return client.ws !== ws
+      })
+      debug(chalk.cyan("[1]close"))
+      debug("clients", runclients)
     })
   })
 
@@ -35,7 +50,18 @@ module.exports = () => {
 /*
 
 create table waiting(
-  email varchar(256)
+  email varchar(256) PRIMARY KEY,
+  sdp varchar(5000) NOT NULL,
+  flag Int(1)
 );
 
+
+create table running(
+  runId Int(16) PRIMARY KEY AUTO_INCREMENT,
+  email varchar(256) NOT NULL,
+  sdp varchar(5000) NOT NULL,
+  clinet varchar(5000) NOT NULL,
+  start date NOT NULL,
+  end date
+);
 */
